a1e01548f7d5af67baa9140c0c13f7ed63105f54
relaxsleepydev <abdullahdev32@gmail.com>
2026-09-19 21:42:58 +0530

    231A

---
+++ Codeforces/231A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a, b, c, count=0;
+    for(int i=1; i<=n; i++)
+    {
+        cin >> a >> b >> c;
+        if((a == 1 && b == 1) || (b == 1 && c == 1) || (c == 1 && a == 1))
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+}
+
+int main()
+{
+    int t=1;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
